116e0615f3676645277bb7e2a9e507ceb7c7444b
KHaerin <coleuyan55@gmail.com>
2024-03-17 14:38:33 +0800

    Adding dropdown into Products/Shop

--- BCC-Ecommerce/src/App.jsx
+++ BCC-Ecommerce/src/App.jsx
@@ -3,6 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./components/About/About";
 import Contact from './components/Contact/Contact';
 import Shop from './components/Shop/Shop';
+import Top from './components/Shop/products/Top';
+import Bottom from './components/Shop/products/Bottom';
 import "./App.css";
 function App() {
  
@@ -18,8 +20,12 @@ function App() {
                                     <li className="nav-item">
                                         <Link to="/" className="nav-link active" aria-current="page">ABOUT</Link>
                                     </li>
-                                    <li className="nav-item">
-                                        <Link to="/shop" className="nav-link">SHOP</Link>
+                                    <li className="nav-item dropdown">
+                                        <Link to="/shop" className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">SHOP</Link>
+                                        <ul className='dropdown-menu dropdown-menu-hover'>
+                                            <li><Link to="/top" className='dropdown-item'>Top</Link></li>
+                                            <li><Link to="/bottom" className='dropdown-item'>Bottm</Link></li>
+                                        </ul>
                                     </li>
                                     <li className="nav-item">
                                         <Link to="/contact" className="nav-link">CONTACT</Link>
@@ -44,6 +50,8 @@ function App() {
             <Route path="/" element={<About />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/top" element={<Top />} />
+            <Route path="/bottom" element={<Bottom />} />
         </Routes>
 
     </BrowserRouter>
