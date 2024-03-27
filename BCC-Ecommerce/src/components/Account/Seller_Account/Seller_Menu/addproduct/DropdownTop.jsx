39b15045bf013117b207a605e5373dbc5112b7ad
KHaerin <coleuyan55@gmail.com>
2024-03-27 16:38:46 +0800

    Update DropdownTop.jsx

--- BCC-Ecommerce/src/components/Account/Seller_Account/Seller_Menu/addproduct/DropdownTop.jsx
+++ BCC-Ecommerce/src/components/Account/Seller_Account/Seller_Menu/addproduct/DropdownTop.jsx
@@ -2,7 +2,16 @@ export default function DropdownTop(){
 
     return(
        <>
-       
+        
+                 <option value="Shirts">Shirts</option>
+                   <option value="Polos">Polos</option>
+                   <option value="Henleys">Henleys</option>
+                   <option value="T-Shirts">T-Shirts</option>
+                   <option value="Dresses">Dresses</option>
+                   <option value="Blouses & Tops">Blouses & Tops</option>
+                   <option value="Sweater & Vests">Sweater & Vests</option>
+                   <option value="Jumpsuits">Jumpsuits</option>
+                   <option value="Blazers">Blazers</option>
        </>
     )
 }
